e7a06a8300f9798cf50b0c6e221aafd62c426870
SaintNevertheless13 <saint.nevertheless13@gmail.com>
2024-10-08 21:16:42 +0500

    Add files via upload

---
+++ module_1_variables (2).ру.py
@@ -0,0 +1,5 @@
+NumberOfHomework = 12
+NumberOfHours = 1.5
+CourseName = 'Python'
+TimeForOneHomework = NumberOfHours / NumberOfHomework
+print(CourseName,',всего задач:',NumberOfHomework,',затрачено часов:',NumberOfHours,',среднее время выполнения',TimeForOneHomework,'часа')
